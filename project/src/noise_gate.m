cf353f5693727f3845adeb4624052e5cee336683
vfasburg <vfasburg@umich.edu>
2015-10-18 16:06:30 -0400

    Fixed noise gate

--- project/src/noise_gate.m
+++ project/src/noise_gate.m
@@ -1,5 +1,6 @@
-function [y, rms] = noise_gate(x, threshold, attackTime, releaseTime, ratio)
+function [y, rms] = noise_gate(x, threshold, attackTime, releaseTime) % , ratio)
     % threshold between 0 and 1, attack/release times in ms
+    tic
     y = zeros(size(x));
     attackSamples = round(attackTime * 160); %convert ms to samples
     releaseSamples = round(releaseTime * 160);
@@ -8,27 +9,23 @@ function [y, rms] = noise_gate(x, threshold, attackTime, releaseTime, ratio)
     
     % calculate RMS in 32 sample chunks
     rms_len = 2048;
-    rms = zeros(size(x));
+    rms = 0;
     rms_samples = ones(rms_len, 1); %holds past x^2 values used in rms
     cur_gain = 0;
     target_attenuation = 0.1;
     for i = 1:length(x)
         %update rms
-
         rms_samples(mod(i, rms_len)+1) = x(i).^2;
-        rms(i) = sqrt(mean(rms_samples));
+        rms = sqrt(mean(rms_samples));
 
-        if(i== 16050)
-            asdf = 1;
-        end
-        if(rms(i) < threshold)
+        if(rms < threshold)
             cur_gain = min(cur_gain + increment, 1);
-            target_attenuation = ratio * ((threshold - rms)/threshold)
+            % target_attenuation = ratio * ((threshold - rms)/threshold);
         else
             cur_gain = max(cur_gain - decrement, 0);
         end
 
         y(i) = (1 - cur_gain) * x(i);
     end
-    
+    toc 
 end
