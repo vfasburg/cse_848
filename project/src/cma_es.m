1430674eb82325db184987a225e704cb34e9861d
vfasburg <vfasburg@umich.edu>
2015-11-13 18:45:57 -0500

    Changed Weights and params
    got a bit better results, down to about. 0.0225

--- project/src/cma_es.m
+++ project/src/cma_es.m
@@ -6,30 +6,30 @@ if nargin<1
 end
 
 % --------------------  Initialization --------------------------------
-% User defined input parameters (need to be edited)
-generationNum = 1;
-counteval = 0;
-N = 8;               % number of objective variables/problem dimension
-xmean = rand(N,1);    % objective variables initial point
-sigma = 0.2;          % coordinate wise standard deviation (step size)
-stopStuckGens = 20;  % stop if fitness doesn't improve after this num gens
-stopeval = 200*12;   % stop after stopeval number of function evaluations
 
 % Strategy parameter setting: Selection
 lambda = 12;                  % number of offspring
 mu = lambda/2;               % number of parents/points for recombination
-weights = log(mu+1/2)-log(1:mu)'; % muXone array for weighted recombination
+weights = sqrt(log(mu+1/2)-log(1:mu))'; % muXone array for weighted recombination
 mu = floor(mu);
 weights = weights/sum(weights);     % normalize recombination weights array
 mueff=sum(weights)^2/sum(weights.^2); % variance-effectiveness of sum w_i x_i
+
+% User defined input parameters (need to be edited)
+N = 8;               % number of objective variables/problem dimension
+xmean = rand(N,1);   % objective variables initial point
+sigma = 0.2;         % step size
+stopStuckGens = 10;  % stop if fitness doesn't improve after this num gens
+stopeval = 300*12;   % stop after 300 generations
+avgfitness = inf * ones(stopStuckGens,1);
 
 % Strategy parameter setting: Adaptation
 cc = (4+mueff/N) / (N+4 + 2*mueff/N);  % time constant for cumulation for C
 cs = (mueff+2) / (N+mueff+5);  % t-const for cumulation for sigma control
 c1 = 2 / ((N+1.3)^2+mueff);    % learning rate for rank-one update of C
 cmu = min(1-c1, 2 * (mueff-2+1/mueff) / ((N+2)^2+mueff));  % and for rank-mu update
-damps = 1 + 2*max(0, sqrt((mueff-1)/(N+1))-1) + cs; % damping for sigma
-% usually close to 1
+damps = 1 + 2*max(0, sqrt((mueff-1)/(N+1))-1) + cs; % damping for sigma, usually close to 1
+
 % Initialize dynamic (internal) strategy parameters and constants
 pc = zeros(N,1);                    % evolution paths for C
 ps = zeros(N,1);                    % evolution paths for sigma
@@ -40,21 +40,24 @@ invsqrtC = B * diag(D.^-1) * B';    % C^-1/2
 eigeneval = 0;                      % track update of B and D
 chiN=N^0.5*(1-1/(4*N)+1/(21*N^2));  % expectation of ||N(0,I)|| == norm(randn(N,1))
 
+generationNum = 1;
+counteval = 0;
+
+% get audio data
 %[clean, fs] = audioread('../audio/I_am_sitting_clean.wav');
 %[noisy, fs] = audioread('../audio/I_am_sitting_dirty.wav');
 [clean, fs] = wavread('../audio/I_am_sitting_clean.wav');
 [noisy, fs] = wavread('../audio/I_am_sitting_dirty.wav');
-scaleFactor = 0.707/(min(max(abs(noisy)), max(abs(clean))))
+scaleFactor = 0.707/(min(max(abs(noisy)), max(abs(clean)))); %scale to -3db
 noisy = noisy * scaleFactor;
 clean = clean * scaleFactor;
 
+%prepare output file
 fileID = fopen('./data.csv', 'w');
 fprintf(fileID, 'run number,generation num,individual num,alpha_wiener,percent_wiener,percent_specsub,threshold,attack,noise len,noise margin,hangover,fitness\n');
 format = '%i,%i,%i,%f,%f,%f,%f,%f,%f,%f,%f,%f\n';
-avgfitness = inf * ones(stopStuckGens,1);
     
 % -------------------- Generation Loop --------------------------------
-% the next 40 lines contain the 20 lines of interesting code
 while counteval < stopeval
     % Generate and evaluate lambda offspring
     tic;
@@ -77,6 +80,7 @@ while counteval < stopeval
     xold = xmean;
     xmean = arx(:,arindex(1:mu))*weights;   % recombination, new mean value
     avgfitness = [avgfitness(1:stopStuckGens-1);mean(arfitness)];
+    
     % Cumulation: Update evolution paths
     ps = (1-cs)*ps ...
         + sqrt(cs*(2-cs)*mueff) * invsqrtC * (xmean-xold) / sigma;
@@ -93,7 +97,7 @@ while counteval < stopeval
     
     % Adapt step size sigma
     sigma = sigma * exp((cs/damps)*(norm(ps)/chiN - 1));
-    
+    fprintf('sigma = %f\n', sigma);
     % Decomposition of C into B*diag(D.^2)*B' (diagonalization)
     if counteval - eigeneval > lambda/(c1+cmu)/N/10  % to achieve O(N^2)
         eigeneval = counteval;
@@ -104,13 +108,11 @@ while counteval < stopeval
     end
     
     % Break if evolution has stopped improving for a while
-    if avgfitness(stopStuckGens) >= min(avgfitness(1:stopStuckGens-1))
+    if avgfitness(stopStuckGens) >= min(avgfitness(1:stopStuckGens-1) + 0.000001)
         break;
     end
     generationNum = generationNum + 1;
 end % while, end generation loop
 fclose(fileID);
 xmin = arx(:, arindex(1)); % Return best point of last iteration.
-% Notice that xmean is expected to be even
-% better.
 end
--- project/src/fitness_manual.m
+++ project/src/fitness_manual.m
@@ -1,14 +1,14 @@
-function data = fitness(clean, noisy, params,  runNum, gen, individual)
+function result = fitness_manual(clean, noisy, params)
     fs = 16000;
     %apply proper ranges to parameters
-    alpha_wiener = params(1) * 2;
-    percent_wiener = params(2) * 4 - 2;
-    percent_specsub = params(3) * 4 - 2;
-    threshold = params(4) * 0.25;
-    attack = params(5) * 10;
-    noise_length = params(6) * 20;
-    noise_margin = params(7) * 20;
-    hangover = params(8) * 20;
+    alpha_wiener = params(1);
+    percent_wiener = params(2);
+    percent_specsub = params(3);
+    threshold = params(4);
+    attack = params(5);
+    noise_length = params(6);
+    noise_margin = params(7);
+    hangover = params(8);
     
     % processed = percent_wiener * WienerNoiseReduction(noisy, fs, alpha_wiener) + (1 - percent_wiener) * spectral_subtraction(noisy, fs, alpha_specsub);
     specsub = percent_specsub * spec_sub_rmr(noisy, fs, noise_length, noise_margin, hangover);
@@ -17,8 +17,8 @@ function data = fitness(clean, noisy, params,  runNum, gen, individual)
     specsub = specsub(1:len);
     weiner = weiner(1:len);
     clean = clean(1:len);
-    
+
     processed = noise_gate(weiner + specsub, threshold, attack, attack);
+    
     result = sum(abs(clean - processed))/len;
-    data = [runNum, gen, individual, alpha_wiener, percent_wiener, percent_specsub, threshold, attack, noise_length, noise_margin, hangover, result];
 end
